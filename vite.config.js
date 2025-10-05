18b09d05e4778c38d11cf0f0919d99bb746f5d79
Umer2000 <umeranis2018@gmail.com>
2025-10-05 23:25:10 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-export default {
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  root: 'frontend',
+  plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
-}
+    outDir: '../dist', // build output will go to root/dist
+  },
+})
